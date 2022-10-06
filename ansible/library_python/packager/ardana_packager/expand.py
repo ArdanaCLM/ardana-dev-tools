fde6e354351bf6dc19187d40e75aa5376a7b0f87
Fergal Mc Carthy <fmccarthy@suse.com>
2022-10-06 13:13:28 -0400

    CVE-2007-4559 fix from GitHub PR (SOC-7471)
    Patching CVE-2007-4559
    ======================
    
    Hi, we are security researchers from the Advanced Research Center
    at Trellix. We have began a campaign to patch a widespread bug named
    CVE-2007-4559. CVE-2007-4559 is a 15 year old bug in the Python tarfile
    package. By using extract() or extractall() on a tarfile object without
    sanitizing input, a maliciously crafted .tar file could perform a
    directory path traversal attack. We found at least one unsantized
    extractall() in your codebase and are providing a patch for you
    via pull request. The patch essentially checks to see if all tarfile
    members will be extracted safely and throws an exception otherwise. We
    encourage you to use this patch or your own solution to secure against
    CVE-2007-4559. Further technical information about the vulnerability
    can be found in this blog.
    
    If you have further questions you may contact us through this projects
    lead researcher Kasimir Schulz.
    
    Change-Id: Ic2b5303b0dcd315ac3477f6cdc5d4207298a3fb2

--- ansible/library_python/packager/ardana_packager/expand.py
+++ ansible/library_python/packager/ardana_packager/expand.py
@@ -107,7 +107,26 @@ def explode(config, spec):
                 m.uname = 'root'
                 m.gname = gname
                 m.mode |= config['extra_mode_bits']
-            tar.extractall(path=target_dir, members=members)
+            def is_within_directory(directory, target):
+
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+
+                return prefix == abs_directory
+
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+
+                tar.extractall(path, members, numeric_owner=numeric_owner)
+
+
+            safe_extract(tar, path=target_dir, members=members)
 
     except Exception as e:
         raise InstallerError(
